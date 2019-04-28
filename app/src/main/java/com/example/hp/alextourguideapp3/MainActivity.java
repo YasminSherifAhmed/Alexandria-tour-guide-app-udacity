c2d9b4339e8d892b21d3e6faf994a2ba9648ff6b
Yasmin Sherif <yasminsherifahmed@gmail.com>
2019-04-28 22:53:56 +0200

    AlexTourGuideApp

---
+++ app/src/main/java/com/example/hp/alextourguideapp3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.hp.alextourguideapp3;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import com.example.hp.alextourguideapp3.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_local_attraction_category);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+}
+
